b82576b640301975cf2305545a52ce3b866adfbb
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-02-10 20:38:23 +0000

    MOBI-1015 Return document with error message when failing to access 'responseXML' property.
    git-svn-id: http://anonsvn.icesoft.org/repo/bridge-support/trunk@39935 8668f098-c06c-11db-ba21-f49e70c34f74

--- src/main/javascript/http.js
+++ src/main/javascript/http.js
@@ -272,7 +272,22 @@ ice.lib.http = ice.module(function(exportAs) {
             });
 
             method(contentAsDOM, function(self) {
-                return nativeRequestResponse.responseXML;
+                try {
+                    return nativeRequestResponse.responseXML;
+                } catch (e) {
+                    var txt = '<error>' + e + '</error>';
+                    var doc;
+                    if (window.DOMParser) {
+                        var parser = new DOMParser();
+                        doc = parser.parseFromString(txt,"text/xml");
+                    } else {
+                        doc = new ActiveXObject("Microsoft.XMLDOM");
+                        doc.async = false;
+                        doc.loadXML(txt);
+                    }
+
+                    return doc;
+                }
             });
 
             method(asString, function(self) {
