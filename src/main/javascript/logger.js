4086cddb57084ea1d91a8a1b0ed0bb8a68d95e66
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-09-01 18:08:35 +0000

    ICE-10785 Disable local storage logging when localStorage variable is missing.
    git-svn-id: http://anonsvn.icesoft.org/repo/bridge-support/trunk@45929 8668f098-c06c-11db-ba21-f49e70c34f74

--- src/main/javascript/logger.js
+++ src/main/javascript/logger.js
@@ -81,7 +81,7 @@ ice.lib.logger = ice.module(function(exportAs) {
             });
 
             method(log, function(self, operation, category, message, exception) {
-                if (localStorage['ice.localStorageLogHandler.enabled']) {
+                if (window.localStorage && window.localStorage['ice.localStorageLogHandler.enabled']) {
                     var formattedMessage = formatOutput(category, message);
                     var priorityName;
                     switch (operation) {
